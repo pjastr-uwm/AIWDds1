992f40332beceeb1df068d9d930ff3b9c8abb90f
pjastr <114875756+pjastr-uwm@users.noreply.github.com>
2025-03-13 12:10:07 +0100

    lab03

---
+++ lab03/task1.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+A = np.array([3, 5, 7, 9])
+B = np.array([2, 4, 6, 8])
+wynik = A / B
+print(wynik)
---
+++ lab03/task2.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+X = np.array([[1, 2, 3], [4, 5, 6]])
+wynik = X + 3
+print(wynik)
