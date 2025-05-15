ef679997c63528d6e4c52620d12b7d22a3f98f5e
pjastr <114875756+pjastr-uwm@users.noreply.github.com>
2025-05-15 12:39:31 +0200

    fix pep8

--- lab11/task1.py
+++ lab11/task1.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 
 x = [24.8, 13.0, 6.7, 55.5]
 c = ['salmon', 'lightblue', 'lightgreen', 'lavender']
-e = [0.1, 0,0,0]
+e = [0.1, 0, 0, 0]
 l = ['Odzieżowe', 'Spożywcze', 'Meblowe', 'Inne']
-plt.pie(x, colors=c, labels=l, explode = e, shadow = True, startangle=15, autopct="%1.1f%%")
+plt.pie(x, colors=c, labels=l, explode=e, shadow=True, startangle=15, autopct="%1.1f%%")
 plt.title("Wyniki sprzedaży - II-IV 2017")
 plt.axis('equal')
 plt.savefig('zad1.png')
--- lab11/task3.py
+++ lab11/task3.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 x = ["Fakt", "Super Express", "Gazeta wyborcza", "Rzeczpospolita"]
 y = [130000, 78000, 40000, 19000]
 c = ["lightcyan", "lightsalmon", "lightsteelblue", "lightpink"]
-plt.bar(x,y, color =c)
+plt.bar(x, y, color=c)
 plt.title("Wyniki sprzedaży dzienników ogólnopolskich za 1Q2023")
 plt.xticks(rotation=30)
 plt.yticks(np.arange(0, 120001, 20000))
