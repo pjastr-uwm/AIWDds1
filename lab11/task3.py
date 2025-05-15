23e091ac45b02c87679b4f0c70c4e90107521828
pjastr <114875756+pjastr-uwm@users.noreply.github.com>
2025-05-15 12:38:24 +0200

    zad3

---
+++ lab11/task3.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = ["Fakt", "Super Express", "Gazeta wyborcza", "Rzeczpospolita"]
+y = [130000, 78000, 40000, 19000]
+c = ["lightcyan", "lightsalmon", "lightsteelblue", "lightpink"]
+plt.bar(x,y, color =c)
+plt.title("Wyniki sprzedaży dzienników ogólnopolskich za 1Q2023")
+plt.xticks(rotation=30)
+plt.yticks(np.arange(0, 120001, 20000))
+plt.tight_layout()
+plt.savefig("zad3.png")
+plt.show()
